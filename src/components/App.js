99012c877f1e1a1be5befbebe796a15bda2f5eb0
mercy-tarus <merciechelah1@gmail.com>
2024-05-13 23:46:20 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
+/** @format */
+
 // create your App component here
+import { useState, useEffect } from "react";
+function App() {
+  const [dog, setDog] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDog(data.message);
+        setLoading(false);
+        //    console.log(result)
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <>
+      <div>
+        {loading ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
+export default App;
